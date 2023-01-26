c2e9e250e86ae28110e3daf2218a2be4125912c8
hippopmonkey <bomingxu1996@163.com>
2023-01-26 18:21:59 +0800

    update

--- get_flops.py
+++ get_flops.py
@@ -1,12 +1,10 @@
 from basicsr.archs.deblur_arch import Deblur
-from basicsr.archs.edvr_arch import EDVR
 from scripts.metrics.calculate_flops import get_model_flops
 from thop import profile
 from ptflops import get_model_complexity_info
 import torch
 
 net = Deblur(num_feat=64, num_block=3, spynet_path='experiments/pretrained_models/flownet/spynet_sintel_final-3d2a1287.pth').cuda()
-# net = EDVR().cuda()
 # input_dim = (10, 3, 256, 256)
 # flops = get_model_flops(net, input_dim, False)
 # print(flops/10**9)
@@ -15,7 +13,7 @@ input_dim = torch.randn(1, 10, 3, 256, 256).cuda()
 flops, params = profile(model=net, inputs=(input_dim, ))
 print(flops/10**9, params/10**6)
 
-# net = Deblur(num_feat=64, num_block=20, spynet_path='experiments/pretrained_models/flownet/spynet_sintel_final-3d2a1287.pth').cuda()
+# net = Deblur(num_feat=64, num_block=3, spynet_path='experiments/pretrained_models/flownet/spynet_sintel_final-3d2a1287.pth').cuda()
 # macs, params = get_model_complexity_info(net, (10, 3, 256, 256), as_strings=True, print_per_layer_stat=True, verbose=True)
 # print('{:<30} {:<8}'.format('Computational complexity:', macs))
 # print('{:<30} {:<8}'.format('Number of patameters:', params))
